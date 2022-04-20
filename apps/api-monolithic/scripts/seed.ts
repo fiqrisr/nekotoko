4e65d4ccb159d712ce008ad17df4acde589d5926
Fiqri Syah Redha <wibulo27@gmail.com>
2022-04-20 22:49:29 +0700

    feat(api-monolithic): add seeder for admin user

---
+++ apps/api-monolithic/scripts/seed.ts
@@ -0,0 +1,33 @@
+import { hash } from 'argon2';
+import { PrismaClient } from '@prisma/client';
+
+async function seed() {
+  console.info('Seeding database...');
+
+  const prisma = new PrismaClient();
+  const username = 'admin';
+  const password = await hash('narkopika');
+  const user = await prisma.user.create({
+    data: {
+      full_name: 'Admin Narkopika',
+      username,
+      password,
+      role: ['admin'],
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { username: user.username },
+    update: {},
+    create: user,
+  });
+
+  void prisma.$disconnect();
+
+  console.info('Seeded database successfully');
+}
+
+seed().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
