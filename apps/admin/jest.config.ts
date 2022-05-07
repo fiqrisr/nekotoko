925a630de23cf397e419998e72ea2cdba3350313
Fiqri Syah Redha <wibulo27@gmail.com>
2022-05-07 19:50:00 +0700

    refactor(admin): fix react import

--- apps/admin/jest.config.ts
+++ apps/admin/jest.config.ts
@@ -1,13 +1,9 @@
 module.exports = {
   displayName: 'admin',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/admin',
--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Refine } from '@pankod/refine-core';
 import {
   Layout,
@@ -8,19 +7,26 @@ import {
 } from '@pankod/refine-antd';
 import routerProvider from '@pankod/refine-react-router-v6';
 import '@pankod/refine-antd/dist/styles.min.css';
+import { authProvider } from '@nekotoko/admin/providers';
 import { dataProvider } from '@nekotoko/admin/providers';
 
 import configs from './configs';
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <Refine
+      authProvider={authProvider(configs.API_URL)}
       routerProvider={routerProvider}
       dataProvider={dataProvider(configs.API_URL)}
       Layout={Layout}
       ReadyPage={ReadyPage}
       notificationProvider={notificationProvider}
       catchAll={<ErrorComponent />}
+      resources={[
+        {
+          name: 'category',
+        },
+      ]}
     />
   );
 };
