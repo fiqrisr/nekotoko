3f94b09a5ab51b80b6578ba34c37b27ab6307870
Fiqri Syah Redha <wibulo27@gmail.com>
2022-05-07 14:28:17 +0700

    feat(admin): add refine package

---
+++ apps/admin/src/configs/index.ts
@@ -0,0 +1,5 @@
+export const configs = {
+  API_URL: import.meta.env.VITE_API_URL,
+};
+
+export default configs;
--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,6 +1,13 @@
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
