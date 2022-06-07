de1b29b913a8cb2e38a2c37e237d4842103cb36c
Fiqri Syah Redha <wibulo27@gmail.com>
2022-06-08 06:29:55 +0700

    add more scripts

---
+++ scripts/cp-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ./apps/api-monolithic/.env.example ./apps/api-monolithic/.env
+cp ./apps/admin/.env.example ./apps/admin/.env
+cp ./apps/pos/.env.example ./apps/pos/.env
+cp ./libs/prisma/monolithic/.env.example ./libs/prisma/monolithic/.env
+
